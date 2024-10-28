90965bde7a8e6174fe4efada0c7405a3f1df48f4
sunnes <ssunne@evolution.com>
2024-10-28 06:31:14 +0200

    fix

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
 
 app="node-hostname"
-version="$1"
 
-helm upgrade $app chart --atomic --create-namespace --namespace $app --timeout 60s --set "appVersion=${version}"
+version="$1"
+if [ -n "$1" ]; then
+  version="--set image.tag=$1"
+fi
+
+helm upgrade $app chart --atomic --create-namespace --namespace $app --timeout 45s ${version}
