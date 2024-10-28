aed5e74f24f286d850590d5df483842a432e5f38
sunnes <ssunne@evolution.com>
2024-10-28 06:07:31 +0200

    test

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
 app="node-hostname"
+version="$1"
 
-helm upgrade $app chart --atomic --create-namespace --namespace $app --timeout 60s
+helm upgrade $app chart --atomic --create-namespace --namespace $app --timeout 60s --value "version=${version}"
