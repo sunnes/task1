7b95d0e2d1618310be1611a5439af8b5bc6cd83c
sunnes <ssunne@evolution.com>
2024-10-29 11:39:10 +0200

    required for the cert-manager validation

--- terraform/k3s_server/main.tf
+++ terraform/k3s_server/main.tf
@@ -35,6 +35,13 @@ resource "aws_security_group" "allow_to_k3s" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     from_port   = 443
     to_port     = 443
